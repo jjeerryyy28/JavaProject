cff68b4ad10127159a2d1b446e4a80504121bd8b
prajw <prajw@_njerry_>
2022-08-20 21:51:58 +0530

    Printing number reverse

--- ProgrammingInJava/src/mypackage/com/PrintNumberReverse.java
+++ ProgrammingInJava/src/mypackage/com/PrintNumberReverse.java
@@ -7,14 +7,14 @@ public class PrintNumberReverse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		 int n=456;
-	       int rev=0;
-	       while(n>0){
-	       rev=rev*10+n%10;
-	       n=n/10;
-	       }
-	        System.out.println(rev);
-	        sc.close();
+		int n = 456;
+		int rev = 0;
+		while (n > 0) {
+			rev = rev * 10 + n % 10;
+			n = n / 10;
+		}
+		System.out.println(rev);
+		sc.close();
 	}
 
 }
